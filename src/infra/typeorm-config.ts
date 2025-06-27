a3f8880a99352f790b9877677003067c813745f6
Tony Sandro <tonysduarte101@gmail.com>
2025-06-27 02:25:28 -0300

    feat: update Docker-compose and typeorm config for improved database setup

--- src/infra/typeorm-config.ts
+++ src/infra/typeorm-config.ts
@@ -6,8 +6,8 @@ import { AccountModel } from "../model/account-model";
 
 export const AppDataSource = new DataSource({
   type: "mysql",
-  host: "localhost",
-  port: 3307,
+  host: process.env.MYSQLDB_HOST,
+  port: Number(process.env.MYSQLDB_LOCAL_PORT),
   username: process.env.MYSQLDB_USER,
   password: process.env.MYSQLDB_ROOT_PASSWORD,
   database: process.env.MYSQLDB_DATABASE,
