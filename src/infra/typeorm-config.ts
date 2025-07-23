d6655ce17e7a2760cec89b946fd5676a64d90726
Tony Sandro <tonysduarte101@gmail.com>
2025-07-23 12:06:04 -0300

    feat: adding settings of Kubernetes of clinical-manager and mysql

--- src/infra/typeorm-config.ts
+++ src/infra/typeorm-config.ts
@@ -1,15 +1,14 @@
 import { DataSource } from "typeorm";
-import "dotenv/config";
 import { PatientModel } from "../model/patient-model";
 import { AnamnesisModel } from "../model/anamnesis-model";
 
 export const AppDataSource = new DataSource({
   type: "mysql",
-  host: process.env.MYSQLDB_HOST,
-  port: Number(process.env.MYSQLDB_LOCAL_PORT),
-  username: process.env.MYSQLDB_USER,
-  password: process.env.MYSQLDB_ROOT_PASSWORD,
-  database: process.env.MYSQLDB_DATABASE,
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   synchronize: true,
   logging: false,
   entities: [PatientModel, AnamnesisModel],
